283d5c6e4898ada3e7db33ba39a976a28c6fae24
Hayatullah Rahnamoon <rahnamoonhayatullah@gamil.com>
2022-01-14 13:28:11 +0800

    save

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://stagapp.near-u.asia'
+  baseUrl: 'https://stagapp.near-u.asia',
+  logoBaseUrl: 'https://stagmedia.near-u.asia/uploads'
 };
