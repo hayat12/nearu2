c96240bbd42b7fd482f116359e6b532e973c7822
Hayatullah Rahnamoon <rahnamoonhayatullah@gamil.com>
2022-01-14 15:46:34 +0800

    save

--- src/app/shipping/app-header/app-header.component.ts
+++ src/app/shipping/app-header/app-header.component.ts
@@ -1,12 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.css']
 })
+
 export class AppHeaderComponent implements OnInit {
-  @Input() title:string = "Nearu";
+  @Input() title: string = "Nearu";
+
+
+  @HostListener('window:unload', [ '$event' ])
+  unloadHandler(event:any) {
+    localStorage.setItem("ev 1", event);
+  }
+
+  @HostListener('window:beforeunload', [ '$event' ])
+  beforeUnloadHandler(event:any) {
+    localStorage.setItem("ev 2", event);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
