76a3d5f7950b966d67419b59012fc14ece365892
Hayatullah Rahnamoon <rahnamoonhayatullah@gamil.com>
2022-01-18 23:09:53 +0800

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EnumScreen } from './shipping/state/shipping.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'nearu';
+  constructor(private router:Router){
+  }
+  ngOnInit(): void {
+    const cart:any = localStorage.getItem(EnumScreen.CART);
+    console.log(cart);
+    if(Object.keys(JSON.parse(cart)).length > 0){
+      this.router.navigate(['nearu/parcels']);
+    }
+  }
 }
