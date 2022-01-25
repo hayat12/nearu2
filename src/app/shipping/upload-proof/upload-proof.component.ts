6518e308407fabb3e8aec783b3bba8a5eec8830e
Hayatullah Rahnamoon <rahnamoonhayatullah@gamil.com>
2022-01-25 18:30:36 +0800

    upload file added

--- src/app/shipping/upload-proof/upload-proof.component.ts
+++ src/app/shipping/upload-proof/upload-proof.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { tap } from 'rxjs/operators';
 import { ServiceService } from '../services/service.service';
 import { SettingHeader } from '../setting-header';
 import { UploadProofENUM } from '../state/shipping.enum';
@@ -13,6 +14,11 @@ import { UploadProofInterface } from '../state/upload/upload-proof.interface';
 export class UploadProofComponent extends SettingHeader implements OnInit {
   selectedFile :FileList;
   fileToUpload: File | null = null;
+  uploadResult = {
+    success: true,
+    message: "",
+    alertType: ""
+  }
   constructor(
     private fb:FormBuilder,
     private _service:ServiceService) {
@@ -27,6 +33,10 @@ export class UploadProofComponent extends SettingHeader implements OnInit {
         fileName: [""],
         fileData: [""],
         uploadType: [UploadProofENUM.PROOF],
+        x1: 20,
+        y1: 20,
+        x2: 20,
+        y2: 20
       }
     );
   }
@@ -50,16 +60,40 @@ export class UploadProofComponent extends SettingHeader implements OnInit {
           fileName = fileName.at(0);
         }
       }
-      this.form.get("fileData")?.patchValue(file);
       this.form.get("fileName")?.patchValue(fileName);
       this.form.get("fileFormat")?.patchValue(fileType);
+      let fileData;
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function (){
+        fileData = reader.result;
+      };
+      setTimeout(() => {
+        // console.log(reader.result);
+        this.form.get("fileData")?.patchValue(reader.result);
+      }, 100);
+
+      // reader.onerror = function (error) {
+      //   console.log('Error: ', error);
+      // };
     }
   }
 
   upload(){
-    // const formData = new FormData();
     const data = this.form.getRawValue();
-    this._service.post_uploadProof(data).subscribe();
+    if (this.form.invalid) {
+      this.uploadResult.success = false;
+      this.uploadResult.message = "Please select the file before upload";
+      this.uploadResult.alertType = "alert alert-danger";
+    }
+    console.log(data);
+    this._service.post_uploadProof(data)
+    .pipe(
+      tap((res)=>console.log()),
+      tap((res)=>console.log())
+    )
+    .subscribe();
   }
 
 }
