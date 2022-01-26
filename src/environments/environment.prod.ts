f0d4dccdf37577fad10d8708c3e51f3e8da0112b
Hayatullah Rahnamoon <rahnamoonhayatullah@gamil.com>
2022-01-26 17:09:16 +0800

    build issue fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://stagapp.near-u.asia',
-  logoBaseUrl: 'https://stagmedia.near-u.asia/uploads'
+  mediaBaseUrl: 'https://stagmedia.near-u.asia',
 };
